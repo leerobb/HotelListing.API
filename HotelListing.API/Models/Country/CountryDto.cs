4b46105064f3f9858a468b7117c097b3540c4c09
leerobb <76053808+leerobb@users.noreply.github.com>
2022-06-27 16:31:29 -0400

    Added Hotels API Endpoint

--- HotelListing.API/Models/Country/CountryDto.cs
+++ HotelListing.API/Models/Country/CountryDto.cs
@@ -5,8 +5,6 @@ namespace HotelListing.API.Models.Country
     public class CountryDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string ShortName { get; set; }
 
         public List<HotelDto> Hotels { get; set; }
     }
--- HotelListing.API/Models/Hotel/HotelDto.cs
+++ HotelListing.API/Models/Hotel/HotelDto.cs
@@ -2,14 +2,9 @@
 
 namespace HotelListing.API.Models.Hotel
 {
-    public class HotelDto
+    public class HotelDto : BaseHotelDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public double Rating { get; set; }
 
-        [ForeignKey(nameof(CountryId))]
-        public int CountryId { get; set; }
     }
 }
